f3e94a2cc1d6c51b892698833df5212af10d15b8
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-04-03 11:37:41 -0300

    Feat: add function comment

--- src/routes/register-for-event.ts
+++ src/routes/register-for-event.ts
@@ -26,6 +26,7 @@ export async function resgisterForEvent(app: FastifyInstance) {
          const { eventId } = request.params;
          const { name, email } = request.body;
 
+         // Check if email is already registered
          const attendeeFromEmail = await prisma.attendee.findUnique({
             where: {
                eventId_email: {
@@ -39,6 +40,7 @@ export async function resgisterForEvent(app: FastifyInstance) {
             throw new Error("This email is already registered for this event");
          }
 
+         // Check if maximum number of attendees is reached
          const [event, amountOfAttendeesForEvent] = await Promise.all([
             prisma.event.findUnique({
                where: {
@@ -60,6 +62,7 @@ export async function resgisterForEvent(app: FastifyInstance) {
             throw new Error("Maximum number of attendees reached");
          }
 
+         // Create new attendee
          const attendee = await prisma.attendee.create({
             data: {
                name,
